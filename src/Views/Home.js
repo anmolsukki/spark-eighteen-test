5e110dac08cbc0d727f223e7c4a73d2c21c3ab62
anmolsukki <anmolsukki94@gmail.com>
2020-05-13 21:27:31 +0530

    checkbox

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,129 +1,93 @@
 import React, { Component } from 'react';
+import Data from "./data.json";
 
 class Home extends Component {
 
     state = {
-        portugal: {},
-        nicaragua: {},
-        islands: {}
+        data: Data
     }
 
-    handlePortugalChange = (e) => {
-        const { portugal } = this.state;
-        portugal[e.target.name] = e.target.checked;
-        if(!portugal[e.target.name]) delete portugal[e.target.name]
-        this.setState({
-            portugal: portugal
-        });
-    }
-
-    handleNicaraguaChange = (e) => {
-        const { nicaragua } = this.state;
-        nicaragua[e.target.name] = e.target.checked;
-        if(!nicaragua[e.target.name]) delete nicaragua[e.target.name]
-        this.setState({
-            nicaragua: nicaragua
-        });
-    }
-
-    handleIslandsChange = (e) => {
-        const { islands } = this.state;
-        islands[e.target.name] = e.target.checked;
-        if(!islands[e.target.name]) delete islands[e.target.name]
-        this.setState({
-            islands: islands
-        });
+    checkChange = (id, checked) => {
+        const jsonData = this.state.data;
+        const checkID = id;
+        const isChecked = checked
+        for(let key in jsonData) {
+            jsonData[key].map(data => {
+                if(data.id === checkID){
+                    data.isChecked = !isChecked;
+                    return data
+                }
+                else {
+                    return data
+                }
+            })
+            this.setState({
+                jsonData
+            })
+        }
     }
 
     render() {
-        const renderPortugal = Object.keys(this.state.portugal)
-        const renderNicaragua = Object.keys(this.state.nicaragua)
-        const renderIslands = Object.keys(this.state.islands)
-        
+        const groupingArr = [];
+        const value = this.state.data;
+        if (value) {
+            for (let key in value) {
+                let first = (
+                    <main>
+                        <h4 className="check-heading">{key}</h4>
+                        {value[key].map((check, index) => {
+                            return(
+                                <label className="customcheck" key={index}>{check.name}
+                                    <input type="checkbox" id={check.id} checked={check.isChecked} onChange={() => this.checkChange(check.id, check.isChecked)} />
+                                    <span htmlFor={check.id} className="checkmark"></span>
+                                </label>
+                            )
+                        })}
+                    </main>
+                )
+                groupingArr.push(first);
+            }
+        }
+
+        const outputArr = []
+        if(value) {
+            for(let key in value) {
+                let output = (
+                    <main>
+                        <h4 className="check-heading">{key}</h4>
+                        {value[key].map((val, index) => {
+                            if(val.isChecked){
+                                return (
+                                    <React.Fragment key={index}>
+                                        <div className="data-list">
+                                            <span className="text">{val.name}</span>
+                                            <span className="cross" onClick={() => this.checkChange(val.id, val.isChecked)}>x</span>
+                                        </div>
+                                    </React.Fragment>
+                                )
+                            }
+                        })}
+                    </main>
+                )
+                outputArr.push(output)
+            }
+        }
+
         return (
             <div className="card-center">
                 <div id="box-1" className="box">
-                    <h4 className="check-heading">Portugal</h4>
-                    <label className="customcheck">Aasiya Jayavant
-                        <input type="checkbox" id="1" name="Aasiya Jayavant" onChange={this.handlePortugalChange} />
-                        <span htmlFor="1" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Luvleen Lawrence
-                        <input type="checkbox" id="2" name="Luvleen Lawrence" onChange={this.handlePortugalChange} />
-                        <span htmlFor="2" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Rey Mibourne
-                        <input type="checkbox" id="3" name="Rey Mibourne" onChange={this.handlePortugalChange} />
-                        <span htmlFor="3" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Cayla Brister
-                        <input type="checkbox" id="4" name="Cayla Brister" onChange={this.handlePortugalChange} />
-                        <span htmlFor="4" className="checkmark"></span>
-                    </label>
-
-                    <h4 className="check-heading">Nicaragua</h4>
-                    <label className="customcheck">Daveedaas Nandi
-                        <input type="checkbox" id="5" name="Daveedaas Nandi" onChange={this.handleNicaraguaChange} />
-                        <span htmlFor="5" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Obasey Chidy
-                        <input type="checkbox" id="6" name="Obasey Chidy" onChange={this.handleNicaraguaChange} />
-                        <span htmlFor="6" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Xenie Dolezelova
-                        <input type="checkbox" id="7" name="Xenie Dolezelova" onChange={this.handleNicaraguaChange} />
-                        <span htmlFor="7" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Ezequlel Dengra
-                        <input type="checkbox" id="8" name="Ezequlel Dengra" onChange={this.handleNicaraguaChange} />
-                        <span htmlFor="8" className="checkmark"></span>
-                    </label>
-
-                    <h4 className="check-heading">Marshall Islands</h4>
-                    <label className="customcheck">Aaron Almaraz
-                        <input type="checkbox" id="9" name="Aaron Almaraz" onChange={this.handleIslandsChange} />
-                        <span htmlFor="9" className="checkmark"></span>
-                    </label>
-                    <label className="customcheck">Jelena Denesova
-                        <input type="checkbox" id="10" name="Jelena Denesova" onChange={this.handleIslandsChange} />
-                        <span htmlFor="10" className="checkmark"></span>
-                    </label>
+                    {groupingArr.map((el, index) => {
+                        return (
+                            <div key={index}>{el}</div>
+                        )
+                    })}
                 </div>
                 <div id="box-2" className="box">
-                    {renderPortugal.length > 0 ? <h4 className="check-heading">Portugal</h4> : null}
-                    {renderPortugal.map((data, index) => {
+                    {outputArr.map((data, index) => {
                         return (
-                            <React.Fragment key={index}>
-                                <div className="data-list">
-                                <span className="text">{data}</span>
-                                    <span className="cross">x</span>
-                                </div>
-                            </React.Fragment>
+                            <div key={index}>{data}</div>
                         )
                     })}
-                    {renderNicaragua.length > 0 ? <h4 className="check-heading">Nicaragua</h4> : null}
-                    {renderNicaragua.map((data, index) => {
-                        return (
-                            <React.Fragment key={index}>
-                                <div className="data-list">
-                                    <span className="text">{data}</span>
-                                    <span className="cross">x</span>
-                                </div>
-                            </React.Fragment>
-                        )
-                    })}
-                    {renderIslands.length > 0 ? <h4 className="check-heading">Marshall Islands</h4> : null}
-                    {renderIslands.map((data, index) => {
-                        return (
-                            <React.Fragment key={index}>
-                                <div className="data-list">
-                                    <span className="text">{data}</span>
-                                    <span className="cross">x</span>
-                                </div>
-                            </React.Fragment>
-                        )
-                    })}
-                    {renderPortugal.length === 0 && renderNicaragua.length === 0 && renderIslands.length === 0 ? <h4 className="no-value">No Value Selected</h4> : null}
                 </div>
             </div>
         )
